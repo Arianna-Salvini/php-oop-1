6287d2ddf640c2f71fd83ee883e6a02130df37e0
“Arianna-Salvini” <arianna.salvini@gmail.com>
2024-04-18 16:39:28 +0200

    Add constructor F

--- index.php
+++ index.php
@@ -5,16 +5,24 @@ class Production
     public $title;
     public $language;
     public $rating;
+
+    public function __construct(string $title, string $language, int $rating)
+    {
+        $this->title = $title;
+        $this->language = $language;
+        $this->rating = $rating;
+    }
 }
 
-$movie1 = new Production();
-$movie1->title = 'Kill Bill - Volume 1';
-$movie1->language = 'English';
-$movie1->rating = 10;
+$movie1 = new Production('Kill Bill - Volume 1', 'English', 10);
+// $movie1->title = 'Kill Bill - Volume 1';
+// $movie1->language = 'English';
+// $movie1->rating = 10;
 
-$movie2 = new Production();
-$movie2->rating = 9;
-$movie2->title = 'Povere creature';
-$movie2->language = 'English';
+$movie2 = new Production('Povere creature', 'English', 9);
+// $movie2->title = 'Povere creature';
+// $movie2->language = 'English';
+// $movie2->rating = 9;
+
 var_dump($movie1);
 var_dump($movie2);
