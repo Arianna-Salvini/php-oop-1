6279ec901c6032b1f0ddd4ca8cc5bfa1f2f671e3
“Arianna-Salvini” <arianna.salvini@gmail.com>
2024-04-19 18:32:27 +0200

    Add genre and genre descriptio to movies array

--- db.php
+++ db.php
@@ -3,81 +3,127 @@ $movies = [
     [
         "title" => "Interstellar",
         "language" => "English",
-        "rating" => 9,
+        "rating" => 8.7,
+        "genre" =>  "Sci-fi/Avventura",
+        "genre_description" => "Sci-fi/Avventura description ",
     ],
     [
         "title" => "Parasite",
         "language" => "Korean",
-        "rating" => 10
+        "rating" => 9,
+        "genre" =>  "Thriller/Commedia ",
+        "genre_description" => "Thriller/Commedia description ",
+
     ],
     [
         "title" => "Inception",
         "language" => "English",
-        "rating" => 9
+        "rating" => 9,
+        "genre" =>  "Sci-fi/Azione",
+        "genre_description" => "Sci-fi/Azione description ",
+
     ],
     [
         "title" => "Pulp Fiction",
-        "language" => "English",
-        "rating" => 9
+        "language" => 9,
+        "genre" =>  "Giallo/Thriller",
+        "genre_description" => "Giallo/Thriller description ",
+
     ],
     [
         "title" => "Kill Bill: Volume 1",
         "language" => "English",
-        "rating" => 9
+        "rating" => 10,
+        "genre" =>  "Azione/Giallo",
+        "genre_description" => "Azione/Giallo description ",
+
     ],
     [
         "title" => "Kill Bill: Volume 2",
         "language" => "English",
-        "rating" => 9
+        "rating" => 8,
+        "genre" =>  "Azione/Giallo",
+        "genre_description" => "Azione/Giallo description ",
+
     ],
     [
         "title" => "Django Unchained",
         "language" => "English",
-        "rating" => 9
+        "rating" => 8.5,
+        "genre" =>  "Western/Azione",
+        "genre_description" => "Western/Azione description ",
+
     ],
     [
         "title" => "Povere Creature",
         "language" => "English",
-        "rating" => 7
+        "rating" => 8.5,
+        "genre" =>  "Commedia",
+        "genre_description" => "Commedia description ",
+
     ],
     [
         "title" => "Quarto Potere",
         "language" => "English",
-        "rating" => 9
+        "rating" => 10,
+        "genre" =>  "Mystery",
+        "genre_description" => "Mystery description ",
+
     ],
     [
         "title" => "Memorie di una Geisha",
         "language" => "English",
-        "rating" => 8
+        "rating" => 7,
+        "genre" =>  "Romantico/Drammatico",
+        "genre_description" => "Romantico/Drammatico description ",
+
     ],
     [
         "title" => "Your Name",
         "language" => "Japanese",
-        "rating" => 9
+        "rating" => 8,
+        "genre" =>  "Animazione/Romantico",
+        "genre_description" => "Animazione/Romantico description ",
+
     ],
     [
         "title" => "La Principessa Mononoke",
         "language" => "Japanese",
-        "rating" => 10
+        "rating" => 9,
+        "genre" =>  "Romantico/Drammatico",
+        "genre_description" => "Romantico/Drammatico description ",
+
     ],
     [
         "title" => "Il Giardino delle Parole",
         "language" => "Japanese",
-        "rating" => 8
+        "rating" => 8,
+        "genre" =>  "Romantico/Drammatico",
+        "genre_description" => "Animazione Romantico/Drammatico description ",
+
     ],
     [
         "title" => "Il Ragazzo e l'Airone",
         "language" => "Japanese",
-        "rating" => 7
+        "rating" => 9.5,
+        "genre" =>  "",
+        "genre_description" => "Genre description ",
+
     ],
     [
         "title" => "C'è Ancora Domani",
         "language" => "Italian",
-        "rating" => 8
+        "rating" => 9,
+        "genre" =>  "Animazione/Avventura",
+        "genre_description" => "Animazione/Avventura description ",
+
     ],
     [
         "title" => "Tàr",
         "language" => "English",
-        "rating" => 9
+        "rating" => 8,
+        "genre" =>  "Musical/Drammatico ",
+        "genre_description" => "Musical/Drammatico  description ",
+
     ],
 ];
