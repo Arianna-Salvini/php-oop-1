6664ad2cb73e1140f99d82e35ae41c2f97eddcf2
“Arianna-Salvini” <arianna.salvini@gmail.com>
2024-04-19 20:46:09 +0200

    add header and footer Add style to header and footer

--- index.php
+++ index.php
@@ -18,6 +18,9 @@ require_once __DIR__ . '/Models/Genre.php';
 </head>
 
 <body class='bg_darkblue'>
+    <header>
+        <h5 class="page_name">Production Ent.</h5>
+    </header>
     <div class="container ">
         <h1 class="text_color_section py-3">Production List</h1>
         <div class="row row-cols-4">
@@ -53,7 +56,9 @@ require_once __DIR__ . '/Models/Genre.php';
         <!-- /.row row-cols-4 -->
     </div>
     <!-- /.container -->
-
+    <footer>
+        <div class="text-center">Production Entertaiment ©</div>
+    </footer>
     <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <script src="https://kit.fontawesome.com/ac8f8c0862.js" crossorigin="anonymous"></script>
